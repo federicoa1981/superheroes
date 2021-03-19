e48014ddec351b984bde650582104e3c3d93902e
fede-ubuntu18 <sistemas.javadoo@gmail.com>
2021-03-19 20:24:32 -0300

    add superheroe entity

---
+++ src/main/java/com/mindata/superheroes/model/Superhero.java
@@ -0,0 +1,20 @@
+package com.mindata.superheroes.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Superhero {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private Integer speed;
+    private Integer strength;
+
+}
