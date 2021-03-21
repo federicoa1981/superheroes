4ffacd83e307fee0c5f694af830fde9ab8484da3
fede-ubuntu18 <sistemas.javadoo@gmail.com>
2021-03-21 16:16:17 -0300

    rename controller

--- src/main/java/com/mindata/superheroes/controller/SuperheroController.java
+++ src/main/java/com/mindata/superheroes/controller/SuperheroController.java
@@ -8,11 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-
 @RestController
 @RequestMapping("/superheroes")
-public class SuperHeroController {
+public class SuperheroController {
 
     @Autowired
     private SuperheroService superheroService;
