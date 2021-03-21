98d1cb15f4407b9bf08be1aaec728a58dea2ea6a
fede-ubuntu18 <sistemas.javadoo@gmail.com>
2021-03-21 12:12:56 -0300

    add tests to searchAll and more superheroes

--- src/main/java/com/mindata/superheroes/controller/SuperHeroController.java
+++ src/main/java/com/mindata/superheroes/controller/SuperHeroController.java
@@ -6,19 +6,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/superheroes")
 public class SuperHeroController {
+    private Superhero s1;
+    private Superhero s2;
+    private Superhero s3;
+
+    public SuperHeroController(){
+        s1 = new Superhero();
+        s1.setId(1L);
+        s1.setSpeed(20);
+        s1.setName("SpiderMan");
+        s1.setStrength(23);
+        s2 = new Superhero();
+        s2.setId(2L);
+        s2.setSpeed(10);
+        s2.setName("Hulk");
+        s2.setStrength(500);
+
+        s3 = new Superhero();
+        s3.setId(3L);
+        s3.setSpeed(1000);
+        s3.setName("Superman");
+        s3.setStrength(200);
+    }
 
     @GetMapping("/{id}")
     public Superhero get(@PathParam("id") Long id){
-        Superhero s =new Superhero();
-        s.setId(1L);
-        s.setSpeed(20);
-        s.setName("SpiderMan");
-        s.setStrength(23);
-        return s;
+        return s1;
+    }
+
+    @GetMapping
+    public List<Superhero> searchAll(){
+        return Arrays.asList(s1, s2, s3);
     }
 
 
--- src/test/java/com/mindata/superheroes/SuperheroesApplicationTests.java
+++ src/test/java/com/mindata/superheroes/SuperheroesApplicationTests.java
@@ -8,9 +8,12 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SuperheroesApplicationTests {
@@ -25,12 +28,9 @@ class SuperheroesApplicationTests {
 
 	@Test
 	public void get() throws Exception {
-		// WHEN
 		ResponseEntity<Superhero> entity = this.testRestTemplate.getForEntity("/superheroes/{id}", Superhero.class, 1L);
 
-		// THEN
 		assertThat(entity.getStatusCode(), is(HttpStatus.OK));
-
 		Superhero superhero = entity.getBody();
 		assertThat(superhero, notNullValue());
 		assertThat(superhero.getId(), is(1L));
@@ -38,6 +38,17 @@ class SuperheroesApplicationTests {
 		assertThat(superhero.getSpeed(), is(20));
 		assertThat(superhero.getStrength(), is(23));
 
+	}
+
+	@Test
+	public void searchAll() throws Exception {
+
+		ResponseEntity<List> entity = this.testRestTemplate.getForEntity("/superheroes", List.class);
+
+		assertThat(entity.getStatusCode(), is(HttpStatus.OK));
+
+		List<Superhero> superheros = entity.getBody();
+		assertThat(superheros, hasSize(3));
 	}
 
 
